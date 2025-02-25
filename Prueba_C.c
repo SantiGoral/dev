a4765e9630d0beb940b250d759bc9e697e8e3511
Santiago Goral <164258911+SantiGoral@users.noreply.github.com>
2025-02-25 13:01:19 -0300

    avance

--- Prueba_C.c
+++ Prueba_C.c
@@ -4,9 +4,17 @@
 void app_calculadora();
 void struct_Menu();
 void _subMenu();
+void contactos();
 
 // DECLARACION DE VARIABLES
 int respuesta_menu
+typedef struct str_contacto{
+  string sc_name [50];
+  string sc_surname[50];
+  double sc_phone;
+}
+//DECLARACION DEL VECTOR
+str_contacto v_contacto[100];
 
 //FUNCION PRINCIPAL
 int main (){
@@ -18,7 +26,7 @@ void struct_Menu(){
   //DECLARACION DE VARIABLE LOCAL
     // SE LE PREGUNTA AL USUARIO QUE DESEA HACER EL SWITCH LO LEEE Y REDIRIGIR
 
-    printf("Que desea hacer?? \n\n");
+    printf("Que desea hacer? \n\n 1-Calculaora 2-Juegos 3-Contactos");
 
     printf("1- Calculadora");
     scanf("%d", &respuesta_menu);
@@ -78,5 +86,38 @@ void app_calculadora(){
 }
 void _subMenu(respuesta_menu){
 
+  int sub_respuesta;
 
+  if(respuesta_menu == 2){ //Menu de juegos
+    printf("Elija el juego: \n\n 1-Buscaminas \n\n 2-Pong \n\n Respuesta:");
+    scanf("%d", &sub_respuesta);
+
+    switch(sub_respuesta){
+      case 1:
+         app_buscamina();
+      break;
+      case 2:
+          app_pong();
+      break:
+    }
+  }
+
+}
+void contacto (){
+   char respuesta = "N/N";
+
+   if(v_contacto[0] == NULL){
+      printf("No posee contactos \n\n");
+   }
+
+   printf("Desea ingreasar un nuevo contacto S/N ");
+   scanf("%s", &respuesta);
+
+   do{
+
+   }while()
+
+    
+
+  }
 }
